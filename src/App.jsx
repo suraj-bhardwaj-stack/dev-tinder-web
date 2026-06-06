8baf12938bde4befc3f7666a30108229c2236e3a
suraj-bhardwaj-stack <surajbhardwaj000007@gmail.com>
2026-06-07 02:00:31 +0530

    Added navbar component using DaisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  
-
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-blue-600">
-          Tailwind is Working 🚀
-        </h1>
-    </div>
-   
+      <Navbar/>
     </>
   )
 }
