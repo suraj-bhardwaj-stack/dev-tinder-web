26e46b57ff7e46e6604fb2048ae83fa8dd762e0b
suraj-bhardwaj-stack <surajbhardwaj000007@gmail.com>
2026-06-07 01:37:16 +0530

    Initial React vite Setup with Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Tailwind is Working 🚀
+        </h1>
+    </div>
+   
+    </>
+  )
+}
+
+export default App
